4e70a58187f688ed7cbeb5d01f6fce2d49b94fd3
Adrian De Guzman <adrian.deguzman@student.ateneo.edu>
2024-03-04 19:35:46 +0800

    Updated models.py, views.py, urls.py, and admin.py

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Recipe, RecipeIngredient
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline]
+
+
+admin.site.register(Recipe, RecipeAdmin)
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,83 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
 
 
 def recipe_list(request):
+    recipes = Recipe.objects.all()
     context = {
-        "recipes": [
-            {
-                "name": "Recipe 1",
-                "link": "/recipe/1"
-            },
-            {
-                "name": "Recipe 2",
-                "link": "/recipe/2"
-            }
-        ]
+        'recipes': recipes
     }
     return render(request, 'ledger/recipe_list.html', context)
 
 
-def recipe_1(request):
+def recipe_detail(request, pk):
+    recipe = Recipe.objects.get(pk=pk)
     context = {
-        "name": "Recipe 1",
-        "ingredients": [
-            {
-                "name": "tomato",
-                "quantity": "3pcs"
-            },
-            {
-                "name": "onion",
-                "quantity": "1pc"
-            },
-            {
-                "name": "pork",
-                "quantity": "1kg"
-            },
-            {
-                "name": "water",
-                "quantity": "1L"
-            },
-            {
-                "name": "sinigang mix",
-                "quantity": "1 packet"
-            }
-        ]
-    }
-    return render(request, 'ledger/recipe_detail.html', context)
-
-
-def recipe_2(request):
-    context = {
-        "name": "Recipe 2",
-        "ingredients": [
-            {
-                "name": "garlic",
-                "quantity": "1 head"
-            },
-            {
-                "name": "onion",
-                "quantity": "1pc"
-            },
-            {
-                "name": "vinegar",
-                "quantity": "1/2cup"
-            },
-            {
-                "name": "water",
-                "quantity": "1 cup"
-            },
-            {
-                "name": "salt",
-                "quantity": "1 tablespoon"
-            },
-            {
-                "name": "whole black peppers",
-                "quantity": "1 tablespoon"
-            },
-            {
-                "name": "pork",
-                "quantity": "1 kilo"
-            }
-        ]
+        'recipe': recipe
     }
     return render(request, 'ledger/recipe_detail.html', context)
