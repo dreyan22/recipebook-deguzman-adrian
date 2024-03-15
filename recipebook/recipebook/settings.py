afdee967b69195edbfaf7326d92c0dd3e977d7ae
Adrian De Guzman <adrian.deguzman@student.ateneo.edu>
2024-03-15 15:10:41 +0800

    Added additional changes on the html files

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -129,6 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/recipes/list'
-LOGOUT_REDIRECT_URL = '/recipes/list'
+LOGOUT_REDIRECT_URL = '/accounts/login'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
